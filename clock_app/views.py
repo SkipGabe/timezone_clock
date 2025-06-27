8d3f4f32e9abf3e19971ae8ce90b081c86ec7671
SkipGabe <gabriel@skipflo.com>
2025-06-27 20:26:14 +0000

    "See changes in db for today's date"

--- clock_app/views.py
+++ clock_app/views.py
@@ -4,3 +4,11 @@ from django.views.decorators.http import require_http_methods
 @require_http_methods(["GET"])
 def clock_view(request):
     return render(request, 'clock.html')
+def clock_view(request):
+    return render(request, 'clock.html')
+def clock_view(request):
+    return render(request, 'clock.html')
+def clock_view(request):
+    return render(request, 'clock.html')
+def clock_view(request):
+    return render(request, 'clock.html')
